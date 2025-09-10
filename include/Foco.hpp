e1c68e01bd165a43db76499949dc2b3e314bcb64
karolgzn <a24310333@live.ceti.mx>
2025-09-10 02:30:48 +0000

    Práctica1

---
+++ include/Foco.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <EstadoFoco.hpp>
+
+class Foco
+{
+private:
+    EstadoFoco estado;
+
+public:
+    Foco() {}
+    ~Foco() {}
+    void Encender() { estado = true; }
+    void Apagar() { estado = false; }
+    EstadoFoco LeerEstado()
+    {
+        return estado;
+    }
+};
